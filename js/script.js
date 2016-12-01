bee4252c8d2b94a0871eec86c3fd258e031b128d
fixed <fixed161@gmail.com>
2016-12-02 00:03:43 +0200

    added js15-16

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(function() {
+            var form = document.querySelector('#searchForm');
+            var searchBtn = document.querySelector('#searchButton');
+            form.addEventListener('keydown', function(e) {
+                if (e.keyCode == 13) {
+                    e.preventDefault();
+                    $("#form_wrapper").removeClass('form_wrapper_center');
+                    Google();
+                }
+            });
+
+            searchBtn.addEventListener("click", function(e) {
+                e.preventDefault();
+                Google();
+            });
+
+});
+
+            function Google() {
+                var searchField = document.querySelector("#searchField").value;
+
+                if (searchField.length > 0) {
+                    searchField = encodeURIComponent(searchField);
+                } else {
+                    return
+                }
+
+                var request = new XMLHttpRequest();
+                request.open('GET', 'https://www.googleapis.com/customsearch/v1?key=AIzaSyD_yotJt04CJfQ4Ibc7wG7uMLP5fEy_zfY&cx=016795527241021289695:jljvvlrwqde&q=' + searchField);
+                request.onreadystatechange = function() {
+                    if (request.readyState == 4) {
+                        if (request.status == 200) {
+                            var data = JSON.parse(request.responseText);
+                            var html = $("#results").html();
+                            var content = tmpl(html, {
+                                data: data.items
+                            });
+                            $('#results').html('');
+                            $('#results').prepend(content);
+                        }
+                    }
+                };
+                request.send();
+            };
