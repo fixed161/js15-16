04d4752b81642091263e03a696ced19410c5d292
fixed <fixed161@gmail.com>
2016-12-06 19:38:18 +0200

    added second task

---
+++ prototype/js/script.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    var Human = {
+      name: "Denis",
+      age: 23,
+      sex: "male",
+      height: 193,
+      weight: 95
+    };
+    console.log(Human);
+
+    
+}
